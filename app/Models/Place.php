07d6d6410e52257b3270677faa59b0d1428fa198
Axel Rindle <mail@axelrindle.de>
2025-10-26 13:52:12 +0000

    feat: create Place entity

---
+++ app/Models/Place.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Place extends Model
+{
+    /** @use HasFactory<\Database\Factories\PlaceFactory> */
+    use HasFactory,
+        HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'latitude',
+        'longitude',
+    ];
+
+    public function warehouses(): HasMany
+    {
+        return $this->hasMany(Warehouse::class);
+    }
+}
