fd925f8b0f55a02b278fa52f69ac113109e3b3bc
Axel Rindle <mail@axelrindle.de>
2025-11-02 20:07:26 +0000

    chore(devcontainer): install ide helper

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @mixin IdeHelperPlace
+ */
 class Place extends Model
 {
     /** @use HasFactory<\Database\Factories\PlaceFactory> */
