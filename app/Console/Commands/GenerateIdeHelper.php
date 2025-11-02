7bd6fc39204394778f711f56572aacdf7e8760ab
Axel Rindle <mail@axelrindle.de>
2025-11-02 20:31:07 +0000

    chore(devcontainer): 🐛 fix ide-helper generation

---
+++ app/Console/Commands/GenerateIdeHelper.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Override;
+
+class GenerateIdeHelper extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ide-helper:composite';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Composite command which detects whether composer is running in dev mode.';
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return bool
+     */
+    #[Override]
+    public function isHidden(): bool
+    {
+        return env('COMPOSER_DEV_MODE', '1') == '0';
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        if ($this->isHidden()) {
+            return;
+        }
+
+        $this->call('ide-helper:generate');
+        $this->call('ide-helper:models', ['-R', '-M']);
+    }
+}
