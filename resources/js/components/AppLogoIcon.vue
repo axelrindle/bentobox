6969f2b393a8a5cec6d4ce46a5fc922b9dc3682d
Lukas Hauenstein <lukas.hauenstein@codeanker.de>
2025-11-02 21:30:57 +0000

    Refactor code structure for improved readability and maintainability

--- resources/js/components/AppLogoIcon.vue
+++ resources/js/components/AppLogoIcon.vue
@@ -1,19 +1,5 @@
-<script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-
-defineOptions({
-    inheritAttrs: false,
-})
-
-interface Props {
-    className?: HTMLAttributes['class'];
-}
-
-defineProps<Props>()
-</script>
-
 <template>
-    <span :class="className" v-bind="$attrs">
+    <span>
         🍱
     </span>
 </template>
