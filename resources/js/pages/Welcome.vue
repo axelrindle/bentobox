40f4476e05f399198b927dff9a97cfa7e95a0769
Lukas Hauenstein <lukas.hauenstein@codeanker.de>
2025-11-02 21:36:40 +0000

    fix: update Button component usage and improve layout in Welcome page

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { Head, Link } from '@inertiajs/vue3'
 import { dashboard, login, register } from '@/routes'
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import AppLogoIcon from '@/components/AppLogoIcon.vue'
+import { Button } from '@/components/ui/button'
 
 withDefaults(
     defineProps<{
@@ -15,30 +16,25 @@ withDefaults(
 
 <template>
     <Head title="Welcome to BentoBox" />
-    <div class="w-screen h-svh bg-sidebar flex flex-col items-center justify-center space-y-12">
+    <div class="w-full h-svh bg-sidebar flex flex-col items-center justify-center space-y-12">
         <AppLogoIcon class="text-6xl" />
         <nav class="flex items-center gap-4">
-            <Link
-                v-if="$page.props.auth.user"
-                :href="dashboard()"
-                class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-            >
-                Dashboard
-            </Link>
+            <Button v-if="$page.props.auth.user">
+                <Link
+
+                    :href="dashboard()"
+                >
+                    Dashboard
+                </Link>
+            </Button>
             <template v-else>
-                <Link
-                    :href="login()"
-                    class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
-                >
-                    Log in
-                </Link>
-                <Link
-                    v-if="canRegister"
-                    :href="register()"
-                    class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                >
-                    Register
-                </Link>
+                <Button>
+                     <Link
+                        :href="login()"
+                    >
+                        Log in
+                    </Link>
+                </Button>
             </template>
         </nav>
     </div>
