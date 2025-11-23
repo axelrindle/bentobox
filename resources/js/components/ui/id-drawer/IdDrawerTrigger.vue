fee96f6e41fa1241e6c2c018d153bc960832916a
Lukas Hauenstein <lukas.hauenstein@codeanker.de>
2025-11-23 14:38:43 +0000

    feat: integrate vaul-vue components and update drawer structure

--- resources/js/components/ui/id-drawer/IdDrawerTrigger.vue
+++ resources/js/components/ui/id-drawer/IdDrawerTrigger.vue
@@ -1,24 +1,51 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
-import { ScanQrCode } from 'lucide-vue-next'
+import type { HTMLAttributes } from "vue";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { ScanQrCode } from "lucide-vue-next";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer";
 
 const props = defineProps<{
-  class?: HTMLAttributes['class']
-}>()
-
+    class?: HTMLAttributes["class"];
+}>();
 </script>
 
 <template>
-  <Button
-    data-sidebar="trigger"
-    data-slot="sidebar-trigger"
-    variant="ghost"
-    size="icon"
-    :class="cn('h-7 w-7', props.class)"
-  >
-    <ScanQrCode />
-    <span class="sr-only">Show Login-ID Code</span>
-  </Button>
+    <Drawer>
+        <DrawerTrigger as-child>
+            <Button
+                data-sidebar="trigger"
+                data-slot="sidebar-trigger"
+                variant="ghost"
+                size="icon"
+                :class="cn('h-7 w-7', props.class)"
+            >
+                <ScanQrCode />
+                <span class="sr-only">Show Login-ID Code</span>
+            </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+            <DrawerHeader>
+                <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                <DrawerDescription>
+                    This action cannot be undone.
+                </DrawerDescription>
+            </DrawerHeader>
+            <DrawerFooter>
+                <Button>Submit</Button>
+                <DrawerClose>
+                    <Button variant="outline"> Cancel </Button>
+                </DrawerClose>
+            </DrawerFooter>
+        </DrawerContent>
+    </Drawer>
 </template>
