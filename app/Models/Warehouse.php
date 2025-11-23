3dcffe06b180b157c9491ddbba81fda69dd940a9
Axel Rindle <mail@axelrindle.de>
2025-11-23 14:46:29 +0000

    fix: add missing relationship

--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @mixin IdeHelperWarehouse
@@ -32,5 +33,10 @@ class Warehouse extends Model
     public function place(): BelongsTo
     {
         return $this->belongsTo(Place::class);
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(Item::class);
     }
 }
